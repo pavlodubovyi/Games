05c1b65928410e47b5e9d9b4d889d0303f9cfde4
pavlodubovyi <133531274+pavlodubovyi@users.noreply.github.com>
2023-08-27 09:48:30 +0200

    next

--- my_first_game.py
+++ my_first_game.py
@@ -1,12 +1,12 @@
 import pygame
 
-pygame.init()
+pygame.init() # automatically initialize modules needed for the game
 
 # Initials
 WIDTH, HEIGHT = 1600, 1000
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Pavlo's First Python Game")
-game_running = True
+game_running = True # flag for while-loop
 
 # colors
 bright_green = (170,255,0)
@@ -19,6 +19,9 @@ paddle_width, paddle_height = 20, 150
 left_paddle_y = right_paddle_y = HEIGHT/2 - paddle_height/2
 left_paddle_x, right_paddle_x = 50 - paddle_width/2, WIDTH - (50 - paddle_width/2)
 
+# paddle speed
+right_paddle_velocity_x = left_paddle_velocity_y = 1, 1
+
 # ball structure
 radius = 25
 ball_x, ball_y = WIDTH/2 - radius, HEIGHT/2 - radius
@@ -30,6 +33,9 @@ while game_running:
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
             game_running = False
+        elif i.type == pygame.KEYDOWN:
+            if i.key == pygame.K_UP: # check if UP key is pressed
+                right_paddle_velocity_x = -1
     
     # movements
     ball_x += ball_velocity_x
@@ -39,5 +45,17 @@ while game_running:
     pygame.draw.circle(window, bright_green, (ball_x, ball_y), radius)
     pygame.draw.rect(window, hot_pink, pygame.Rect(left_paddle_x, left_paddle_y, paddle_width, paddle_height))
     pygame.draw.rect(window, red, pygame.Rect(right_paddle_x, right_paddle_y, paddle_width, paddle_height))
-
     pygame.display.update() # this line makes the ball and paddles visible
+
+    # actions if ball goes out the screen
+    if ball_y <= 0 + radius or ball_y >= HEIGHT - radius: # top-bottom boundaries
+        ball_velocity_y *= -1 # don't know why there's no difference between '*= -1' and '= -1'
+    if ball_x >= WIDTH: # reset the ball position to the center, if it goes out of the screen (RIGHT)
+        ball_x, ball_y = WIDTH/2 - radius, HEIGHT/2 - radius
+        ball_velocity_x *= -1
+        ball_velocity_y *= -1
+    if ball_x <= 0: # reset the ball position to the center, if it goes out of the screen (LEFT)
+        ball_x, ball_y = WIDTH/2 - radius, HEIGHT/2 - radius
+        ball_velocity_x, ball_velocity_y = 0
+
+
